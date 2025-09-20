da5476a5a73e4589c87e8964b7887c995f4ffd7c
Raúl <raulmdaw@gmail.com>
2025-09-20 13:40:30 +0200

    Reconexión automática

--- src/app/core/constants/player.constants.ts
+++ src/app/core/constants/player.constants.ts
@@ -5,4 +5,8 @@ export const PLAYER_CONSTANTS = {
   PLAYER_GET_BY_ID: 'player:getById',
   PLAYER_FOUND: 'player:found',
   PLAYER_NOT_FOUND: 'player:notFound',
+
+  PLAYER_IDENTIFY: 'player:identify',
+  PLAYER_IDENTIFIED: 'player:identified',
+  PLAYER_RECONNECT: 'player:reconnect',
 } as const;
