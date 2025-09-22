07b0a4390fd4e6ba65edd76ac6f1d5544f98716e
Raúl <raulmdaw@gmail.com>
2025-09-22 18:37:05 +0200

    Función salir de sala

--- src/app/features/room/room.component.ts
+++ src/app/features/room/room.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RoomStoreService } from '../../core/services/room-store.service';
 import { GameStoreService } from '../../core/services/game-store.service';
 import { ApiPlayerService } from '../../core/services/api/api.player.service';
@@ -15,10 +15,12 @@ export class RoomComponent implements OnInit {
   private roomStore = inject(RoomStoreService);
   private gameStore = inject(GameStoreService);
   private apiPlayer = inject(ApiPlayerService);
+  private router = inject(Router);
 
   roomId!: string;
   room = this.roomStore.currentRoom;
   player = this.apiPlayer.player;
+  confirmingLeave = false;
 
   ngOnInit() {
     this.roomId = this.route.snapshot.paramMap.get('id')!;
@@ -46,5 +48,25 @@ export class RoomComponent implements OnInit {
 
   goHome() {
     this.roomStore.goHome();
+  }
+
+  leaveRoom() {
+    this.confirmingLeave = true;
+  }
+
+  confirmLeave() {
+    if (this.roomId) {
+      const player = this.player();
+      if (player) {
+        this.roomStore.leaveRoom(this.roomId, player);
+      }
+    }
+
+    this.confirmingLeave = false;
+    this.router.navigate(['/room-list']);
+  }
+
+  cancelLeave() {
+    this.confirmingLeave = false;
   }
 }
