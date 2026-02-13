a636cc5c329988dbb9bd111ad6ad10ffda3f8853
Raúl <raulmdaw@gmail.com>
2026-02-13 19:20:11 +0100

    se aplica titlecase para los nombres de jugadores

--- src/app/features/room/components/room-players/room-players.component.ts
+++ src/app/features/room/components/room-players/room-players.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input, Signal } from '@angular/core';
 import { Room } from '@core/models/room.model';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-room-players',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './room-players.component.html',
   styleUrl: './room-players.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
